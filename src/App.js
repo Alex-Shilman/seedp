50820fa396a292dac7e377e1267efd21e465f84d
Alex Shilman <ashilman@CA2269.local>
2018-01-03 13:41:47 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">DATA PLATFORM DASHBOARD</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          WELCOME
         </p>
       </div>
     );
