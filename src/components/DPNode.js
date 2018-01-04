9852aa663a6ce57f4b04b2197b7922a29971c21d
Paul <secondsight001@hotmail.com>
2018-01-04 09:33:48 -0500

    remove unused method

--- src/components/DPNode.js
+++ src/components/DPNode.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class DPNode extends Component {
-  _handleClick = () => {
-    alert('Yo, what up!');
-  }
-
   render() {
     return (
       <div className="DPNode">
