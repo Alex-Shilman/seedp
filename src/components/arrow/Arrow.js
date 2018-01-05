4f2e4800d1353f11070cc59ba06329c643c20b25
Alex Shilman <ashilman@CA2269.local>
2018-01-05 11:46:21 -0500

    fixed class names

--- src/components/arrow/Arrow.js
+++ src/components/arrow/Arrow.js
@@ -6,8 +6,8 @@ const Arrow = ({rotate}) => (
   <div className={classNames('DPArrow', rotate)}>
     <svg id="arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.65 10.23">
       <title>Untitled-6</title>
-      <line id="head" class="arrow-head" y1="5.22" x2="143.99" y2="5.22" />
-      <polyline id="path" class="arrow-line" points="139.18 0.35 143.94 5.12 139.18 9.88" />
+      <line id="head" className="arrow-head" y1="5.22" x2="143.99" y2="5.22" />
+      <polyline id="path" className="arrow-line" points="139.18 0.35 143.94 5.12 139.18 9.88" />
     </svg>
   </div>
 );
