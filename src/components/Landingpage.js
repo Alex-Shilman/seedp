27dd1c1cd5b17962cb532e4dc073701cdac1f053
Paul <secondsight001@hotmail.com>
2018-01-05 15:49:32 -0500

    login form

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import logo from '../assets/system-config-services.png';
 
 export default () => (
   <section className="landing">
     <div className="login-form">
+      <div className="logo">
+         <img src={logo} width="64px" height="64px" alt="" />{' '}seeDP
+      </div>
       <Form>
           <FormGroup>
-            <Label className="login-field" for="exampleUsername">Username</Label>
+            <Label className="login-label" for="exampleUsername">Username</Label>
             <Input className="login-field" type="text" name="username" id="exampleUsername" placeholder="username" />
           </FormGroup>
           <FormGroup>
-            <Label className="login-field" for="examplePassword">Password</Label>
-            <Input type="password" name="password" id="examplePassword" placeholder="password" />
+            <Label className="login-label" for="examplePassword">Password</Label>
+            <Input className="login-field" type="password" name="password" id="examplePassword" placeholder="password" />
           </FormGroup>
-          <Link to="/data-platform"><Button >SignOn</Button></Link>
-        </Form>
+          <Link to="/data-platform"><Button onClick={() => false} >SignOn</Button></Link>
+      </Form>
     </div>
   </section>
-  
 );
