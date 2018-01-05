74881fb48849402efc11d34be5644616b38457ff
sjuste <SJuste@cainc.com>
2018-01-05 13:58:08 -0500

    Add all icons

--- src/components/DPTopics.js
+++ src/components/DPTopics.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 import pipe from '../assets/pipe.svg';
 
+import PipeIcon from './pipeIcon/PipeIcon';
+
 class DPTopics extends Component {
   constructor(props) {
     super(props);
@@ -23,7 +25,7 @@ class DPTopics extends Component {
     const buttonId = `${name}-topics-btn`;
     return (
       <div className="DPTopics">
-        <img src={pipe} alt="" />
+        <PipeIcon status={message ? 'warning' : 'healthy'}/>
         <p className="topic-text"><br/><br/><Button  id={buttonId} onClick={this.toggle}>{group.dispName}</Button></p>
         <p className="error-text">{message}</p>
         <Popover placement="left" isOpen={this.state.popoverOpen} target={buttonId} toggle={this.toggle}>
