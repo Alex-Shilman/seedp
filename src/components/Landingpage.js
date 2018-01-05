0b0bd37087a0e2cb4d79cb012581453577314dd5
Paul <secondsight001@hotmail.com>
2018-01-05 14:39:31 -0500

    WIP

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 export default () => (
   <section className="landing">
-    <p>Landing Page</p>
-    <Link to="/data-platform">Enter at your own risk</Link>
+    <div className="login-form">
+      <Form>
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          </FormGroup>
+          <Link to="/data-platform">Enter at your own risk</Link>
+        </Form>
+    </div>
   </section>
   
 );
