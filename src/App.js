da3d5bd1c963bf6646e731e217cddaa9228fddfb
Paul <secondsight001@hotmail.com>
2018-01-06 04:10:13 -0500

    add sign out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import { Container } from 'reactstrap';
 import DPConnectorView from './components/DPConnectorView';
 import Timer from './components/Timer';
 import Notification from './components/Notification';
 import Landingpage from './components/Landingpage';
 import DPNodeView from './components/DPNodeView';
+import signout from './assets/signout.png';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
@@ -18,8 +19,11 @@ export default class App extends Component {
           <section>
             <header className="App-header">
               <h1 className="App-title">
-               NINTENDO DASHBOARD{' '}
+                NINTENDO DASHBOARD{' '}
                 <Timer />
+                <Link to="/">
+                  <img src={signout} alt="sign out" width="32" height="32" />
+                </Link>
               </h1>
             </header>
             <Notification />
