48120d85da98a87fd695325ff3ad2b97e4c0d950
Paul <secondsight001@hotmail.com>
2018-01-08 09:01:29 -0500

    styling

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from '../assets/system-config-services.png';
+import { Link } from 'react-router-dom';
 
 export default ({width, height, ...ownProps}) => (
-  <div className="logo" {...ownProps}>
-    <img src={logo} width={width} height={height} alt="Logo" />{' '}seeDP
-  </div>
+  <Link to="/">
+    <div className="logo" {...ownProps}>
+      <img src={logo} width={width} height={height} alt="Logo" />{' '}seeDP
+    </div>
+  </Link>
 );
